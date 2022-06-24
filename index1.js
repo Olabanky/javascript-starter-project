3e2650acba04130e6add67cccb637f54788700b2
Olabanky <103018713+Olabanky@users.noreply.github.com>
2022-06-24 16:32:24 +0100

    Introduction to  javascript starter project

---
+++ index1.js
@@ -0,0 +1,29 @@
+
+// To take input
+
+// function getDetails(name, height, country) {
+
+//     var name = prompt('Enter your name:  ')
+
+//     var height = prompt('Enter your height in ft:  ')
+
+//     var country = prompt('Please enter your country:  ')
+
+
+//     console.log("My name is " + name);
+//     console.log("My height is " + height);
+//     console.log("I am from " + country)
+// }
+
+// getDetails()
+
+function getDetails(name, height, country) {
+    var name = "Azeez Olayinka Bankole";
+    var height = "5.5 ft";
+    var country = "Nigeria";
+    console.log("My name is " + name);
+    console.log("My height is " + height);
+    console.log("I am from " + country)
+}
+
+getDetails()
